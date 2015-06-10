7b8aaef1096db414d3db1f4016b7685cf3f47110
DaliaPapuc <dalia.papuc@gamil.com>
2015-06-10 13:33:25 +0300

    whatever

--- WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -6,6 +6,13 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
+    //ljhjlhjkhhj
+    //git add . - adauga
+    //git commit -m "comentariu" pune un nume commituului
+    //git pull - ia chestiile noi de pe repo
+    //rezolva conflictele in caz ca exista
+    //git push pune ce ai facut tu si face merge
+    //in caz ca aici nu vrea git push si tipa mai bagi un git add . si git commit -m "acelasicomentariucamaidevreme" si git push
     static class Program
     {
         /// <summary>
